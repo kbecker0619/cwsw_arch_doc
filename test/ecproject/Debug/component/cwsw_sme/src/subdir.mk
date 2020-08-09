8f0bb61e577c51310a8344a00359265cc27f1182
Kevin Becker <Kevin.L.Becker@Live.com>
2020-08-08 20:22:59 -0400

    "Fix" buildability from within Eclipse on different computer

--- test/ecproject/Debug/component/cwsw_sme/src/subdir.mk
+++ test/ecproject/Debug/component/cwsw_sme/src/subdir.mk
@@ -4,7 +4,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-C:/_Projects/cwsw_evqueue_test/component/cwsw_sme/src/cwsw_sme.c 
+D:/_Projects/cwsw_evqueue_test/component/cwsw_sme/src/cwsw_sme.c 
 
 OBJS += \
 ./component/cwsw_sme/src/cwsw_sme.o 
@@ -14,10 +14,10 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-component/cwsw_sme/src/cwsw_sme.o: C:/_Projects/cwsw_evqueue_test/component/cwsw_sme/src/cwsw_sme.c
+component/cwsw_sme/src/cwsw_sme.o: D:/_Projects/cwsw_evqueue_test/component/cwsw_sme/src/cwsw_sme.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -std=c11 -D"XPRJ_Win_MinGW_Debug=1" -DUSE_NOTIFICATION_EVENTS=1 -DDEMO_APP=1 -I"C:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_lib/inc" -I"C:/_Projects/cwsw_evqueue_test/test/ecproject/../app/cfg" -I"C:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_eventqueue/cwsw_event/inc" -I"C:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_eventqueue/cwsw_evqueue/inc" -I"C:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_eventqueue/cwsw_evqueue_ex/inc" -I"C:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_clock_services/cwsw_clock/inc" -I"C:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_clock_services/cwsw_swtimer/inc" -I"C:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_board/bd_gtk" -I"C:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_board/common" -I"C:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_arch/inc/i386" -I"C:/_Projects/cwsw_evqueue_test/test/ecproject/../../component/cwsw_sme/inc" -I"C:/_Projects/cwsw_evqueue_test/test/ecproject/../app" -I"D:\_Programs\git-sdk-64\mingw64/include/gtk-3.0" -I"D:\_Programs\git-sdk-64\mingw64/include/glib-2.0" -I"D:\_Programs\git-sdk-64\mingw64/lib/glib-2.0/include" -I"D:\_Programs\git-sdk-64\mingw64/include/pango-1.0" -I"D:\_Programs\git-sdk-64\mingw64/include/cairo" -I"D:\_Programs\git-sdk-64\mingw64/include/gdk-pixbuf-2.0" -I"D:\_Programs\git-sdk-64\mingw64/include/atk-1.0" -O0 -g3 -Wall -Wextra -Wconversion -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -std=c11 -D"XPRJ_Win_MinGW_Debug=1" -DUSE_NOTIFICATION_EVENTS=1 -DDEMO_APP=1 -I"D:/_Projects/cwsw_evqueue_test/test/ecproject/../app" -I"D:/_Projects/cwsw_evqueue_test/test/ecproject/../app/cfg" -I"D:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_board/common" -I"D:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_eventqueue/cwsw_evqueue_ex/inc" -I"D:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_eventqueue/cwsw_event/inc" -I"D:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_eventqueue/cwsw_evqueue/inc" -I"D:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_clock_services/cwsw_swtimer/inc" -I"D:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_clock_services/cwsw_clock/inc" -I"D:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_lib/inc" -I"D:/_Projects/cwsw_evqueue_test/test/ecproject/../../component/cwsw_sme/inc" -I"D:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_board/bd_gtk" -I"D:/_Projects/cwsw_evqueue_test/test/ecproject/../libs/cwsw_arch/inc/i386" -I"E:\_Programs\git-sdk-64\mingw64/include/gtk-3.0" -I"E:\_Programs\git-sdk-64\mingw64/include/glib-2.0" -I"E:\_Programs\git-sdk-64\mingw64/lib/glib-2.0/include" -I"E:\_Programs\git-sdk-64\mingw64/include/pango-1.0" -I"E:\_Programs\git-sdk-64\mingw64/include/cairo" -I"E:\_Programs\git-sdk-64\mingw64/include/gdk-pixbuf-2.0" -I"E:\_Programs\git-sdk-64\mingw64/include/atk-1.0" -O0 -g3 -Wall -Wextra -Wconversion -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
